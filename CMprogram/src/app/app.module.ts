2eb08c31b835249c40bcccfb9725173dba45eff6
erikqb3 <erikqb3@gmail.com>
2022-10-06 18:25:11 -0600

    Should work, saving for now

--- CMprogram/src/app/app.module.ts
+++ CMprogram/src/app/app.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { DocumentsComponent } from './documents/documents.component';
-import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
-import { MessageListComponent } from './messages/message-list/message-list.component';
-import { MessageItemComponent } from './messages/message-list/message-item/message-item.component';
-import { MessageEditComponent } from './messages/message-edit/message-edit.component';
-import { DocumentListComponent } from './documents/document-list/document-list.component';
-import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
-import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactDetailComponent } from './components/contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './components/contacts/contact-list/contact-list.component';
+import { DocumentsComponent } from './components/documents/documents.component';
+import { ContactItemComponent } from './components/contacts/contact-list/contact-item/contact-item.component';
+import { MessageListComponent } from './components/messages/message-list/message-list.component';
+import { MessageItemComponent } from './components/messages/message-list/message-item/message-item.component';
+import { MessageEditComponent } from './components/messages/message-edit/message-edit.component';
+import { DocumentListComponent } from './components/documents/document-list/document-list.component';
+import { DocumentItemComponent } from './components/documents/document-list/document-item/document-item.component';
+import { DocumentDetailComponent } from './components/documents/document-detail/document-detail.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { DocumentDetailComponent } from './documents/document-detail/document-de
     DocumentListComponent,
     DocumentItemComponent,
     DocumentDetailComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule
